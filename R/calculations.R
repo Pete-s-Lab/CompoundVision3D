0bf9d5ebd6cf2659d9891e82c068c9e35bbc7d5d
Peter T. Ruehr <peter.ruehr@gmail.com>
2025-12-01 23:09:36 +0100

    moved get_ROI_coordinates()

--- R/calculations.R
+++ R/calculations.R
@@ -136,52 +136,6 @@ translate_ROIs <- function(df,
   return(df_translated)
 }
 
-#' Extract ROI coordinates from crop_log file
-#'
-#' Rotates 3D point cloud according to one defined vector so that this vector
-#' is aligned to one of the global coordinate system axes.
-#'
-#' @param df A tibble containing coordinates in columns `x, y, z`.
-#' @param line_points A 2x3 tibble containing coordinates of line to align the 
-#' point cloud to. Must contain one row per point and columns `x, y, z`.
-#' @param axis A character string defining the global axis to align to. Must be 
-#' `x`, `y`, or `z`.
-#' @return Returns a tibble with the aligned coordinates in columns `x, y, z`.
-#' @importFrom dplyr add_row
-#'
-#' @export
-#' @examples
-#' xxx: add example and change above descsriptionand parameters
-#'
-get_ROI_coordinates <- function(ROIs,
-                                crop_log_data){
-  results = tibble(ROI = character(),
-                   x_coord = numeric(), y_coord = numeric(), 
-                   x_length = numeric(), y_length = numeric(), 
-                   z1_coord = numeric(), z2_coord = numeric())
-  i=1
-  for(i in 1:length(ROIs)){
-    curr_ROI_name <- ROIs [i]
-    curr_ROI <- crop_log_data$val[which(grepl(paste0("ROI_", curr_ROI_name), crop_log_data$var))]
-    ROI_coords <- strsplit(gsub("\\);", "",
-                                strsplit(curr_ROI, "\\(")[[1]][2]), ", ")[[1]]
-    x_coord <- as.numeric(ROI_coords[1])
-    y_coord <- as.numeric(ROI_coords[2])
-    x_length <- as.numeric(ROI_coords[3])
-    y_length <- as.numeric(ROI_coords[4])
-    
-    z1_coord <- as.numeric(crop_log_data$val[which(grepl(paste0("z_first_", curr_ROI_name), crop_log_data$var))])
-    z2_coord <- as.numeric(crop_log_data$val[which(grepl(paste0("z_last_", curr_ROI_name), crop_log_data$var))])
-    
-    results = results %>%  
-      add_row(ROI = curr_ROI_name,
-              x_coord = x_coord, y_coord = y_coord, 
-              x_length = x_length, y_length = y_length, 
-              z1_coord = z1_coord, z2_coord = z2_coord)
-  }
-  return(results)
-}
-
 
 
 
